c07d7549cbebe616eab614aea9f30148bb5cacdc
Ngocji <dngocx@gmail.com>
2020-08-24 13:14:52 +0700

    Refactor st1

--- jibinding/src/main/java/com/jibinding/adapter/viewholder/BindFlexibleExpandableViewHolder.kt
+++
@@ -1,8 +0,0 @@
-package com.jibinding.adapter.viewholder
-
-import androidx.databinding.ViewDataBinding
-import com.jibase.iflexible.adapter.FlexibleAdapter
-import com.jibase.iflexible.viewholder.simple.FlexibleExpandableViewHolder
-
-open class BindFlexibleExpandableViewHolder(val binding: ViewDataBinding, adapter: FlexibleAdapter<*>, isStickyHeader: Boolean = false) :
-        FlexibleExpandableViewHolder(binding.root, adapter, isStickyHeader)
