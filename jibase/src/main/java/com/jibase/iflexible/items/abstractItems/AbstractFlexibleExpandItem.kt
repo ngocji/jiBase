22c6ed6cde67609f6048d55b2bf16a8c544b919c
Ngocji <dngocx@gmail.com>
2020-08-25 11:55:34 +0700

    Fix expandable item

--- jibase/src/main/java/com/jibase/iflexible/items/abstractItems/AbstractFlexibleExpandItem.kt
+++ jibase/src/main/java/com/jibase/iflexible/items/abstractItems/AbstractFlexibleExpandItem.kt
@@ -5,7 +5,7 @@ import com.jibase.iflexible.items.interfaceItems.IExpandable
 import com.jibase.iflexible.items.interfaceItems.IFlexible
 import com.jibase.iflexible.viewholder.FlexibleExpandableViewHolder
 
-abstract class AbstractFlexibleExpandItem<VH : FlexibleExpandableViewHolder, S : IFlexible<*>>(private var preSubItems: MutableList<S> = mutableListOf()) : AbstractFlexibleItem<VH>(), IExpandable<VH, S> {
+abstract class AbstractFlexibleExpandItem<VH : FlexibleExpandableViewHolder, S : IFlexible<*>>(private var subItems: MutableList<S> = mutableListOf()) : AbstractFlexibleItem<VH>(), IExpandable<VH, S> {
     /* Flags for FlexibleAdapter */
     open var isExpand = false
 
@@ -22,36 +22,38 @@ abstract class AbstractFlexibleExpandItem<VH : FlexibleExpandableViewHolder, S :
 
     override fun getExpansionLevel() = 0
 
+    override fun getSubItems(): List<S> = subItems
+
     /*-------------------*/
     /* SUB ITEMS METHODS */
     /*-------------------*/
 
     fun hasSubItems(): Boolean {
-        return preSubItems.isNotEmpty()
+        return subItems.isNotEmpty()
     }
 
     fun setSubItems(subItems: List<S>): AbstractFlexibleExpandItem<VH, S> {
-        this.preSubItems = subItems.toMutableList()
+        this.subItems = subItems.toMutableList()
         return this
     }
 
     fun addSubItems(position: Int, subItems: List<S>): AbstractFlexibleExpandItem<VH, S> {
-        if (preSubItems has position) {
-            preSubItems.addAll(position, subItems)
+        if (this.subItems has position) {
+            this.subItems.addAll(position, subItems)
         } else {
-            this.preSubItems.addAll(subItems)
+            this.subItems.addAll(subItems)
         }
         return this
     }
 
     fun addSubItem(subItem: S): AbstractFlexibleExpandItem<VH, S> {
-        preSubItems.add(subItem)
+        subItems.add(subItem)
         return this
     }
 
     fun addSubItem(position: Int, subItem: S): AbstractFlexibleExpandItem<VH, S> {
-        if (preSubItems has position) {
-            preSubItems.add(position, subItem)
+        if (subItems has position) {
+            subItems.add(position, subItem)
         } else {
             addSubItem(subItem)
         }
@@ -59,34 +61,34 @@ abstract class AbstractFlexibleExpandItem<VH : FlexibleExpandableViewHolder, S :
     }
 
     fun getSubItemsCount(): Int {
-        return preSubItems.size
+        return subItems.size
     }
 
 
     fun getSubItem(position: Int): S? {
-        return if (preSubItems has position) preSubItems[position]
+        return if (subItems has position) subItems[position]
         else null
     }
 
     fun getSubItemPosition(subItem: S): Int {
-        return preSubItems.indexOf(subItem)
+        return subItems.indexOf(subItem)
     }
 
     operator fun contains(subItem: S): Boolean {
-        return preSubItems.contains(subItem)
+        return subItems.contains(subItem)
     }
 
     fun removeSubItem(item: S?): Boolean {
-        return item != null && preSubItems.remove(item)
+        return item != null && subItems.remove(item)
     }
 
     fun removeSubItems(subItems: List<S>?): Boolean {
-        return subItems != null && this.preSubItems.removeAll(subItems)
+        return subItems != null && this.subItems.removeAll(subItems)
     }
 
     fun removeSubItem(position: Int): Boolean {
-        if (preSubItems has position) {
-            preSubItems.removeAt(position)
+        if (subItems has position) {
+            subItems.removeAt(position)
             return true
         }
         return false
