7660be619a5204df212bb85d5742b6c83d782756
Ngocji <dngocx@gmail.com>
2022-06-09 11:35:27 +0700

    - update diffcallback

--- jibase/src/main/java/com/jibase/iflexible/helpers/FlexibleDiffCallback.kt
+++ jibase/src/main/java/com/jibase/iflexible/helpers/FlexibleDiffCallback.kt
@@ -3,11 +3,22 @@ package com.jibase.iflexible.helpers
 import androidx.recyclerview.widget.DiffUtil
 import com.jibase.iflexible.items.interfaceItems.IFlexible
 
-open class FlexibleDiffCallback<T : IFlexible<*>>(private var oldList: List<T>, var newList: List<T>) : DiffUtil.Callback() {
+open class FlexibleDiffCallback<T : IFlexible<*>>(
+    var oldList: List<T>,
+    var newList: List<T>
+) : DiffUtil.Callback() {
 
-    fun setItems(oldList: List<T>, newList: List<T>) {
+    open fun setItems(oldList: List<T>, newList: List<T>) {
         this.oldList = oldList
         this.newList = newList
+    }
+
+    open fun areItemTheSame(old: T?, new: T?): Boolean {
+        return old?.getIdView() == new?.getIdView()
+    }
+
+    open fun areContentsTheSame(old: T?, new: T?): Boolean {
+        return old == new
     }
 
     override fun getOldListSize() = oldList.size
@@ -15,10 +26,16 @@ open class FlexibleDiffCallback<T : IFlexible<*>>(private var oldList: List<T>, 
     override fun getNewListSize() = newList.size
 
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return oldList[oldItemPosition].getIdView() == newList[newItemPosition].getIdView()
+        return areItemTheSame(
+            oldList.getOrNull(oldItemPosition),
+            newList.getOrNull(newItemPosition)
+        )
     }
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return oldList[oldItemPosition] == newList[newItemPosition]
+        return areContentsTheSame(
+            oldList.getOrNull(oldItemPosition),
+            newList.getOrNull(newItemPosition)
+        )
     }
 }
