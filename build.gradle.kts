78771d7ebf37a6b743d6fb0ad25a64db826d0f58
Doan Van Ngoc/대리/메가존_MZ HMI <dvngoc@mz.co.kr>
2024-10-23 16:37:42 +0700

    Update java 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ allprojects {
 
     tasks.withType(KotlinCompile::class.java).configureEach {
         kotlinOptions {
-            jvmTarget = "1.8"
+            jvmTarget = "11"
         }
     }
 
@@ -28,8 +28,8 @@ allprojects {
             }
 
             compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_1_8
-                targetCompatibility = JavaVersion.VERSION_1_8
+                sourceCompatibility = JavaVersion.VERSION_11
+                targetCompatibility = JavaVersion.VERSION_11
             }
         }
     }
